ad1a04a4bdb71547b9c944daa07d897bde32bff1
kdr <dogelove@ddfactory.kr>
2023-08-08 01:59:46 +0900

    api test 코드 적용

--- Image_search/lib/data/pixabay_api.dart
+++ Image_search/lib/data/pixabay_api.dart
@@ -5,12 +5,17 @@ import 'package:image_search/model/Photo.dart';
 import 'package:http/http.dart' as http;
 
 class PixabayApi extends PhotoApiRepository {
-  final baseUrl = 'https://pixabay.com';
-  final key = '38550726-0dfa18b64c6d5292f82824859';
+  static const baseUrl = 'https://pixabay.com';
+  static const key = '38550726-0dfa18b64c6d5292f82824859';
 
   @override
-  Future<List<Photo>> fetch(String query) async {
-    final response = await http.get(Uri.parse(
+  Future<List<Photo>> fetch(String query, {http.Client? client}) async {
+    // if (client == null) {
+    //   client = http.Client();
+    // }
+    client ??= http.Client();
+
+    final response = await client.get(Uri.parse(
         '$baseUrl/api/?key=$key&q=$query&image_type=photo'));
     Map<String, dynamic> jsonResponse = jsonDecode(response.body);
     Iterable hits = jsonResponse['hits'];
