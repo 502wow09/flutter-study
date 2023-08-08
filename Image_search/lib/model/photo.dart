9d7961405a9fe549b4fd6c4ddaa241b55da4597d
kdr <dogelove@ddfactory.kr>
2023-08-09 03:57:31 +0900

    JsonSerializable, Equatable -> freezed 라이브러리 하나로 해결 가능

--- Image_search/lib/model/photo.dart
+++ Image_search/lib/model/photo.dart
@@ -1,28 +1,15 @@
-import 'package:equatable/equatable.dart';
-import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-//아래 명시된 파일 하나 생성한다는 의미
+part 'photo.freezed.dart';
 part 'photo.g.dart';
 
-@JsonSerializable()
-class Photo extends Equatable {
-  final int id;
-  final String tags;
-
-  //api의 field name이 이상하게 되어있으면 재정의 가능
-  @JsonKey(name: 'largeImageURL')
-  final String largeImageUrl;
-
-  const Photo({
-    required this.id,
-    required this.tags,
-    required this.largeImageUrl,
-  });
+@freezed
+class Photo with _$Photo {
+  const factory Photo({
+    required int id,
+    required String tags,
+    required String largeImageURL,
+  }) = _Photo;
 
   factory Photo.fromJson(Map<String, dynamic> json) => _$PhotoFromJson(json);
-
-  Photo toJson() => _$PhotoFromJson(this as Map<String, dynamic>);
-
-  @override
-  List<Object?> get props => [id];
 }
