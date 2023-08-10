40634bb75c8ff8a0699f92134ab13af4d0b95108
kdr <dogelove@ddfactory.kr>
2023-08-10 20:10:20 +0900

    state 설정 후 로딩바 띄우기, freezed 라이브러리로 교체

--- Image_search/test/ui/home_view_model_test.dart
+++ Image_search/test/ui/home_view_model_test.dart
@@ -13,7 +13,7 @@ void main() {
 
     final result = fakeJson.map((e) => Photo.fromJson(e)).toList();
 
-    expect(viewModel.photos, result);
+    expect(viewModel.state.photos, result);
   });
 }
 
