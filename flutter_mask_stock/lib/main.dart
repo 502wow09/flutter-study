834a76d2b336d93ae5d8173c396ee899960de4fc
kdr <dogelove@ddfactory.kr>
2023-08-11 03:10:15 +0900

    mask save

---
+++ flutter_mask_stock/lib/main.dart
@@ -0,0 +1,159 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'model/store.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Store> stores = [];
+  bool isLoading = true;
+
+  Future<List<Store>> fetch() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    // String url = 'https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json?lat=37.29965674401107&lng=126.99743969036126&m=5000';
+    String urlString =
+        'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json';
+    Uri url = Uri.parse(urlString);
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final jsonResult = jsonDecode(utf8.decode(response.bodyBytes));
+      final jsonStores = jsonResult['stores'];
+
+      setState(() {
+        stores.clear();
+        jsonStores.forEach((e) {
+          final store = Store.fromJson(e);
+          stores.add(store);
+        });
+        isLoading = false;
+      });
+
+      return stores.toList();
+    } else {
+      return [];
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetch();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('마스크 재고 있는 곳 : ${stores.where((e) {
+          return e.remainStat == 'plenty' ||
+              e.remainStat == 'some' ||
+              e.remainStat == 'few';
+        }).length}곳'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: fetch,
+          )
+        ],
+      ),
+      body: isLoading
+          ? loadingWidget()
+          : ListView(
+              children: stores.where((e) {
+                return e.remainStat == 'plenty' ||
+                    e.remainStat == 'some' ||
+                    e.remainStat == 'few';
+              }).map((e) {
+                return ListTile(
+                  title: Text(e.name.toString()),
+                  subtitle: Text(e.addr.toString()),
+                  trailing: _buildRemainStatWidget(e),
+                );
+              }).toList(),
+            ),
+    );
+  }
+
+  Widget _buildRemainStatWidget(Store store) {
+    var remainStat = '판매중지';
+    var description = '판매중지';
+    var color = Colors.black;
+    switch (store.remainStat) {
+      case 'plenty':
+        remainStat = '충분';
+        description = '100개 이상';
+        color = Colors.green;
+        break;
+      case 'some':
+        remainStat = '보통';
+        description = '30 ~ 100개';
+        color = Colors.yellow;
+        break;
+      case 'few':
+        remainStat = '부족';
+        description = '2 ~ 30개';
+        color = Colors.red;
+        break;
+      case 'empty':
+        remainStat = '소진임박';
+        description = '1개 이하';
+        color = Colors.black;
+        break;
+    }
+
+    return Column(
+      children: <Widget>[
+        Text(
+          remainStat,
+          style: TextStyle(color: color, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          description,
+          style: TextStyle(color: color),
+        ),
+      ],
+    );
+  }
+
+  Widget loadingWidget() {
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('정보를 가져오는 중'),
+          CircularProgressIndicator(),
+        ],
+      ),
+    );
+  }
+}
